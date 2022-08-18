41bb9de6880492c61aa85d0df0cc8577d7a4449a
Ali-jalili <mrali97jl@gmail.com>
2022-08-18 16:43:28 +0430

    Add File Project

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+let menu = document.querySelector(".mene")
+let menuBtn = document.querySelector(".header_icon")
+let menuBtnIcon = document.querySelector(".header_icon i")
+
+menuBtn.addEventListener("click" ,function (){
+    menu.style.left = "0"
+})
